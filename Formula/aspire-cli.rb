f6d055fcb0cae97ee96bba4112d2fc1e95cb9a84
Carmine Marsocci <carmine@Carmines-Mac-mini.local>
2026-05-28 23:39:15 +0200

    initial commit

---
+++ Formula/aspire-cli.rb
@@ -0,0 +1,29 @@
+class AspireCli < Formula
+  desc "Aspire CLI"
+  homepage "https://github.com/microsoft/aspire"
+  version "13.3.5"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/microsoft/aspire/releases/download/v13.3.5/aspire-cli-osx-arm64-13.3.5.tar.gz"
+      sha256 "7ae8181bec6dee12d1cfb039dd201d7cfb71b51c674b0f06a853f402742afbd3"
+    else
+      url "https://github.com/microsoft/aspire/releases/download/v13.3.5/aspire-cli-osx-x64-13.3.5.tar.gz"
+      sha256 "dafcf1633a6f2970c017d75287f502b0a033d65ff6acac02899f2761f8d16c62"
+    end
+  end
+
+  on_linux do
+    url "https://github.com/microsoft/aspire/releases/download/v13.3.5/aspire-cli-linux-musl-x64-13.3.5.tar.gz"
+    sha256 "b3a7c77490ca9bf1fd433b6e3b100bea7bfa32d1529904d6617a04281e562f3c"
+  end
+
+  def install
+    bin.install "aspire-cli"
+  end
+
+  test do
+    assert_match "aspire-cli", shell_output("#{bin}/aspire-cli --version")
+  end
+end
