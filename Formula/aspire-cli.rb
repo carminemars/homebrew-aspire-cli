e69c696a355f5f51b851bc4b9f168572726459db
Carmine Marsocci <carmine@Carmines-Mac-mini.local>
2026-05-29 00:06:17 +0200

    fix test assertion

--- Formula/aspire-cli.rb
+++ Formula/aspire-cli.rb
@@ -24,6 +24,6 @@ class AspireCli < Formula
   end
 
   test do
-    assert_match "aspire", shell_output("#{bin}/aspire --version")
+    assert_match version.to_s, shell_output("#{bin}/aspire --version")
   end
 end
